2c13c2d0322a8879b38de5470d3ab40fe0d753c3
Pippin Barr <pippin.barr@gmail.com>
2018-01-03 14:04:48 -0500

    Light Experiments: Added a simple Day/Night version where the directional light is rotated around its x-axis in order to create a sense of time passing. Ended up accelerating it to move quite quickly just to see the effect.
    ? Deeply uncomplicated. Just involved writing a simple script and attaching to the directional light in the scene. Big whoop. It's uninspiring as an experience, although moving shadows are nice in general. Just the bare _fact_ of the shadows being dynamic is kind of incredible really - that there is that much power in a line of code etc. I think the dynamics of the Headlight-based shadows were more interesting and had more emotion. The only thing I'd say though is that that day-night cycle is interesting to think about because it relates to one of Unity's neat "freebies" in terms of the skybox etc., a dawn and dusk and so forth (even seems to have a moonlit night as far as I can tell?
    
    ? Obviously adding a sense of time passing (and maybe particularly a lot of time passing quickly) could be tied to some ideas around war and the amount of time that it takes up, the amount of destruction etc. Could serve as some kind of metaphorical understanding of the timeless war? That feels pretty heavy-handed to me though... just trying to run with the materials, right? Could see it as a connection to survival-based games where night is more threatening and you need to find shelter. The idea of tanks getting cold is funny. The idea of tanks needing to go to bed? Like, they play during the day but then at night they have to go home, eat dinner, say their prayers, go to sleep, so they can play again in the morning? How would you enforce that kind of experience for the player? How would they know their tank has to go home? What would home look like? Add two little houses the tanks go back to? Just some text on screen telling them to stop fighting and go home to bed? And maybe their guns are disabled from dusk? So there's no point staying out? Or maybe there's just a timer before you get in trouble with your parents and you lose the game that way? Otherwise it just goes on? What if you could only play on the weekend and have to go to school the rest of the week? Have to answer some math questions and some multichoice questions. That's getting pretty fanciful, but there's a design idea there in terms of time that I like... I guess I need to track some of this idea stuff somewhere separate again. Sheeeeeeeeesh. I will.
    
    Okay that was more generative than expected.

---
+++ Assets/Scripts/Light/DayNightLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(1f,0f,0f));
+	}
+}
