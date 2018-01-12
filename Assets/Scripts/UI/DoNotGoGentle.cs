0ab2fa6271a506d13b543fa624441407cfd213bb
Pippin Barr <pippin.barr@gmail.com>
2018-01-11 22:23:33 -0500

    Do Not Go Gentle: Added basic ability to display lines of the poem over the top of the scene (using existing UI function) and dimming the directional light with no ambient
    ? There's something here in terms of the relationship of the poem to the light. I think more can be done with the relationship of the poem to the tanks. Slower? Not shooting? Just shooting? What does it mean for a tank to rage against the dying of the light? What could that mean?
    
    ? Wonder if it's better if the dying of the light is smoother with a Lerp instead of staccato... might feel more dreadful. Can you desaturate at the same time? Probably.

---
+++ Assets/Scripts/UI/DoNotGoGentle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DoNotGoGentle : MonoBehaviour
+{
+
+    public Light light;
+    public float lineTime = 1f;
+
+    private string [] poem = new string[] {
+"Do not go gentle into that good night,",
+"Old age should burn and rave at close of day;",
+"Rage, rage against the dying of the light.",
+"Though wise men at their end know dark is right,",
+"Because their words had forked no lightning they",
+"Do not go gentle into that good night.",
+"Good men, the last wave by, crying how bright",
+"Their frail deeds might have danced in a green bay,",
+"Rage, rage against the dying of the light.",
+"Wild men who caught and sang the sun in flight,",
+"And learn, too late, they grieved it on its way,",
+"Do not go gentle into that good night.",
+"Grave men, near death, who see with blinding sight",
+"Blind eyes could blaze like meteors and be gay,",
+"Rage, rage against the dying of the light."
+    };
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        StartCoroutine(DisplayPoem());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator DisplayPoem() {
+        for (int i = 0; i < poem.Length; i++) {
+            text.text = poem[i].ToUpper();
+            light.intensity -= (1f/(poem.Length+2));
+            yield return new WaitForSeconds(lineTime);
+        }
+    }
+}
