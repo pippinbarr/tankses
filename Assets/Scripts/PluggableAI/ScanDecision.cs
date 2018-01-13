92c783a5f2cfee017c2f973a70f9aeddb54b59c7
Pippin Barr <pippin.barr@gmail.com>
2018-01-13 13:28:43 -0500

    AI scene and tanks are fully functional
    ? So it works. The only thing remaining is to work out a clean/nice way of integrating it into the main branch I suppose? Or non-clean? Does it matter how clean? Unsure.

---
+++ Assets/Scripts/PluggableAI/ScanDecision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/Scan")]
+public class ScanDecision : TankDecision {
+
+	public override bool Decide(StateController controller)
+    {
+        bool noEnemyInSight = Scan(controller);
+        return noEnemyInSight;
+    }
+
+    private bool Scan(StateController controller)
+    {
+        controller.navMeshAgent.isStopped = true;
+        controller.transform.Rotate(0, controller.enemyStats.searchingTurnSpeed * Time.deltaTime, 0);
+        return controller.CheckIfCountdownElapsed(controller.enemyStats.searchDuration);
+    }
+}
